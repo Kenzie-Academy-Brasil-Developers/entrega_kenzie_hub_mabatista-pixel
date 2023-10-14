558b181f5aeb642b7c85240c223d60ab1eb853f8
Matheus Batista <mabatista_10@hotmail.com>
2023-10-14 20:29:14 -0300

    Feat: Implementada estilização página de registro

--- src/components/Forms/RegisterForm/index.jsx
+++ src/components/Forms/RegisterForm/index.jsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import { Input } from "../../Input";
 import { Select } from "../../Select";
+import styles from "./styles.module.scss";
 
 export const RegisterForm = () => {
 
@@ -11,34 +12,38 @@ export const RegisterForm = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit(submit)}>
-            <h1>Crie sua conta</h1>
-            <span>Rápido e grátis, vamos nessa</span>
-            <div>
-                <Input label="Nome" type="text" {...register("name")} required
-                    placeholder="Digite aqui seu nome..." />
-            </div>
-            <div>
-                <Input label="Email" type="email" {...register("email")} required placeholder="Digite aqui seu email" />
+        <section>
+            <form onSubmit={handleSubmit(submit)}>
+                <div className={styles.registerFormContainer}>
+                    <h1>Crie sua conta</h1>
+                    <span>Rápido e grátis, vamos nessa</span>
+                    <div>
+                        <Input label="Nome" type="text" {...register("name")} required
+                            placeholder="Digite aqui seu nome..." />
+                    </div>
+                    <div>
+                        <Input label="Email" type="email" {...register("email")} required placeholder="Digite aqui seu email" />
 
-            </div>
-            <div>
-                <Input label="Senha" type="password" {...register("password")} required placeholder="Digite aqui sua senha" />
-            </div>
-            <div>
-                <Input label="Confirmar senha" type="password" {...register("password")} required placeholder="Confirmar senha" />
-            </div>
-            <div>
-                <Input label="Bio" type="text" {...register("bio")} required placeholder="Fale sobre você" />
-            </div>
-            <div>
-                <Input label="Contato" type="text" {...register("contact")} required placeholder="Opção de contato" />
+                    </div>
+                    <div>
+                        <Input label="Senha" type="password" {...register("password")} required placeholder="Digite aqui sua senha" />
+                    </div>
+                    <div>
+                        <Input label="Confirmar senha" type="password" {...register("password")} required placeholder="Confirmar senha" />
+                    </div>
+                    <div>
+                        <Input label="Bio" type="text" {...register("bio")} required placeholder="Fale sobre você" />
+                    </div>
+                    <div>
+                        <Input label="Contato" type="text" {...register("contact")} required placeholder="Opção de contato" />
 
-            </div>
-            <div>
-                <Select />
-            </div>
-            <button type="submit">Criar conta</button>
-        </form>
+                    </div>
+                    <div className={styles.selectDiv}>
+                        <Select />
+                    </div>
+                    <button className="btn-primary-negative" type="submit">Cadastrar</button>
+                </div>
+            </form>
+        </section >
     )
 }
--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -5,17 +5,19 @@ export const Select = () => {
     const [selectedModule, setSelectedModule] = useState("module1")
 
     return (
-        <label>
-            Selecionar módulo
-            <select name="selectedModule"
-                value={selectedModule} onChange={(e) => setSelectedModule(e.target.value)}>
-                <option value="module1">Módulo 1</option>
-                <option value="module2">Módulo 2</option>
-                <option value="module3">Módulo 3</option>
-                <option value="module4">Módulo 4</option>
-                <option value="module5">Módulo 5</option>
-                <option value="module6">Módulo 6</option>
-            </select>
-        </label>
+        <div>
+            <label>
+                Selecionar módulo
+                <select name="selectedModule"
+                    value={selectedModule} onChange={(e) => setSelectedModule(e.target.value)}>
+                    <option value="module1">Primeiro Módulo </option>
+                    <option value="module2">Segundo Módulo </option>
+                    <option value="module3">Terceiro Módulo </option>
+                    <option value="module4">Quarto Módulo </option>
+                    <option value="module5">Quinto Módulo </option>
+                    <option value="module6">Sexto Módulo </option>
+                </select>
+            </label>
+        </div>
     )
 }
