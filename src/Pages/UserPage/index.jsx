dc63b0882ec100efa1e53e669b8eb2e593e9b2ea
Matheus Batista <mabatista_10@hotmail.com>
2023-10-14 21:29:40 -0300

    Feat: Aplicada estilização página do usuário

--- src/Pages/UserPage/index.jsx
+++ src/Pages/UserPage/index.jsx
@@ -1,10 +1,11 @@
-import  logo  from "../../assets/logo.svg";
+import logo from "../../assets/logo.svg";
 import { Link } from "react-router-dom";
+import styles from "./style.module.scss";
 
 export const UserPage = () => {
     return (
         <>
-            <header>
+            <header className={styles.userHeader}>
                 <div>
                     <img src={logo} alt="Logo Kenzie Hub" />
                 </div>
@@ -14,7 +15,17 @@ export const UserPage = () => {
                 </button>
             </header>
             <main>
-                <h1>Página do usuário</h1>
+                <div className={styles.userMain}>
+                    <div className={styles.borderTopDiv}>
+                        <h1 className="title1">Olá, Matheus Batista</h1>
+                        <span>Terceiro módulo (React)</span>
+                    </div>
+                    <div>
+                        <h1 className="title1">Que pena! Estamos em desenvolvimento :( </h1>
+                        <p className="paragraph">Nossa aplicação está em desenvolvimento, em breve teremos novidades</p>
+                    </div>
+
+                </div>
             </main>
         </>
     )
