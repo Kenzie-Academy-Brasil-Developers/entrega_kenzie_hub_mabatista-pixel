759fc5198957a5238374e85fb77d81d524565117
Matheus Batista <mabatista_10@hotmail.com>
2023-10-17 17:04:41 -0300

    Feat: Realizada refatoração para acomodar linha horizontal na página de usuário

--- src/Pages/UserPage/index.jsx
+++ src/Pages/UserPage/index.jsx
@@ -1,33 +1,35 @@
 import logo from "../../assets/logo.svg";
 import { Link } from "react-router-dom";
 import styles from "./style.module.scss";
+import { Line } from "../../components/Line";
 
 export const UserPage = () => {
     return (
         <>
             <div className={styles.pageContainer}>
                 <header className={styles.userHeader}>
-                    <div>
+                    <div className={styles.headerDivButton}>
                         <img src={logo} alt="Logo Kenzie Hub" />
+                        <button>
+                            <Link to="/">Sair
+                            </Link>
+                        </button>
                     </div>
-                    <button>
-                        <Link to="/">Sair
-                        </Link>
-                    </button>
+                    <hr className={styles.customLine} />
+                    
+                    <div className={styles.headerDivInfo}>
+                        <h1 className="title1">Olá, Matheus Batista</h1>
+                        <span>Terceiro módulo (React)</span>
+                    </div>
                 </header>
 
-                <hr className={styles.customLine}/>
+                <hr className={styles.customLine} />
 
-                <main>
-                    <div className={styles.userMain}>
-                        <div className={styles.borderTopDiv}>
-                            <h1 className="title1">Olá, Matheus Batista</h1>
-                            <span>Terceiro módulo (React)</span>
-                        </div>
+                <main className={styles.userMain}>
+                    <div className={styles.boxContainer}>
 
-                <hr className={styles.customLine}/>
-                
-                        <div>
+
+                        <div className={styles.notificationDiv}>
                             <h1 className="title1">Que pena! Estamos em desenvolvimento :( </h1>
                             <p className="paragraph">Nossa aplicação está em desenvolvimento, em breve teremos novidades</p>
                         </div>
---
+++ src/components/Line/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export const Line = () => {
+    return(
+        <div>
+            <hr className={styles.mediumLine} />
+        </div>
+    )
+}
